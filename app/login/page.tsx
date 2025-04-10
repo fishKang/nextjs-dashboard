ce2a806412d605e0b65d509355d44c9d1bd362d4
fishKang <707024922@qq.com>
2025-04-10 15:37:00 +0800

    feat(core): 添加客户、发票、仪表盘页面及UI组件，并更新布局和字体

---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-blue-500">I'm blue!</h1>
+   <h1>
+    <a>
+      hello world!
+    </a>
+   </h1>
+   <h2>
+    <Link href="/">
+      fff
+    </Link>
+   </h2>
+   </div>
+  );
+}
